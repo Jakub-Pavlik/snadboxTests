94df8ad7ca8ea8392ad2ed845a100873352e0121
Jakub-Pavlik <39928909+Jakub-Pavlik@users.noreply.github.com>
2019-08-08 17:02:15 +0200

    Create 11.js

---
+++ 11.js
@@ -0,0 +1,12 @@
+let nums = [];
+while (true){
+    let input = window.prompt('Enter number: ')
+    if (!input) break;
+    nums.push(input);
+}
+nums.shift();
+let reversed = [];
+for(let i=0; i<nums.length; i++){
+    reversed.unshift(nums[i]);
+}
+console.log(reversed.join('_'));
